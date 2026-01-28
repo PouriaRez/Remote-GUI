5e3d353795c384ecf2815b1535c0ebef42fab5bd
PouriaRez <rezaeizadehpouria@gmail.com>
2026-01-28 14:33:47 -0800

    Added Backend logic for pulling kibana url

--- CLI/local-cli-backend/plugins/kibana/kibana_router.py
+++ CLI/local-cli-backend/plugins/kibana/kibana_router.py
@@ -1,23 +1,21 @@
 # ELK Plugin
 # Provides access to ELK stack and Kibana dashboard
 
-
 from fastapi import APIRouter
 from pydantic import BaseModel
-
+import os
 # Create the API router
 api_router = APIRouter(prefix="/kibana", tags=["Kibana"])
 
+# Default Kibana URL
+DEFAULT_KIBANA_URL = os.getenv("KIBANA_URL", "http://localhost:5601/app/dashboards")
+
 # Request model
 class KibanaRequest(BaseModel):
-    # name: type
-    pass
+    url: str
 
 
-'''
-Endpoints needed:
-    - Need to grab Kibana URL
-'''
+
 # API endpoints
 @api_router.get("/")
 async def kibana_info():
@@ -25,5 +23,4 @@ async def kibana_info():
 
 @api_router.get("/url")
 async def get_kibana_url():
-    kibana_url = ''
-    return {"url": kibana_url}
+    return {"url": DEFAULT_KIBANA_URL}
