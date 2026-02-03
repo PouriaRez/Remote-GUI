a334c57a69be77321604c80226e51a58b894733d
Pouria Rezaei <rezaeizadehpouria@gmail.com>
2026-02-02 17:14:24 -0800

    Created blank CLI page

---
+++ CLI/local-cli-fe-full/src/plugins/cli/CliPage.js
@@ -0,0 +1,11 @@
+// Plugin metadata - used by the plugin loader
+export const pluginMetadata = {
+  name: 'CLI',
+  icon: null,
+  // 📊 => Use icon?
+};
+const CliPage = () => {
+  return <div>CliPage</div>;
+};
+
+export default CliPage;
